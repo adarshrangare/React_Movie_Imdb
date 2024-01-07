be0479347008045941801717aa09fe7af25b5a65
Adarsh Rangare <akrangare@gmail.com>
2024-01-08 03:39:20 +0530

    retry

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { store } from "./store/store.js";
 import { Provider } from "react-redux";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
